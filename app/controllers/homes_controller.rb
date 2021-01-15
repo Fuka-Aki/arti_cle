e1c59e81f32bf54eeb1194e74f1920f89b4298ff
Fuka-Aki <f.akiyama0926@gmail.com>
2021-01-16 01:32:37 +0900

    ユーザー機能の実装

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,10 @@
+class HomesController < ApplicationController
+  before_action :move_to_index
+  def index
+  end
+  def move_to_index
+    if user_signed_in?
+      redirect_to rooms_path
+    end
+  end
+end
