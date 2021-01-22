081357554d1d58fe94cce4f66d5c68b53a977973
Fuka-Aki <f.akiyama0926@gmail.com>
2021-01-22 16:58:26 +0900

    モデル単体テスト

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @room = FactoryBot.build(:room)
+  end
+
+  describe 'ルーム新規作成' do
+    context 'ルームが作成できる場合' do
+      it '全ての項目が入力されていれば、作成できる' do
+        expect(@room).to be_valid
+      end
+    end
+    context 'ルームが作成できない場合' do
+      it 'artist_nameが空だと作成できない' do
+        @room.artist_name = nil
+        @room.valid?
+        expect(@room.errors.full_messages).to include("アーティスト名を入力してください")
+      end
+      it 'genre_idが空だと作成できない' do
+        @room.genre_id = nil
+        @room.valid?
+        expect(@room.errors.full_messages).to include("ジャンルを入力してください")
+      end
+      it 'genre_idで0が選択されている場合は作成できない' do
+        @room.genre_id = nil
+        @room.valid?
+        expect(@room.errors.full_messages).to include("ジャンルは数値で入力してください")
+      end
+      it 'imageが空だと作成できない' do
+        @room.image = nil
+        @room.valid?
+        expect(@room.errors.full_messages).to include("画像を入力してください")
+      end
+      it 'userが紐付いていないと保存できないこと' do
+        @room.user = nil
+        @room.valid?
+        expect(@room.errors.full_messages).to include("ユーザーを入力してください")
+      end
+    end
+  end
 end
