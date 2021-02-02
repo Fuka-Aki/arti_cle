503daf2c495304e80b5735ea07a5f0a7f0f93afb
Fuka-Aki <f.akiyama0926@gmail.com>
2021-02-02 12:29:08 +0900

    DB_clear

--- db/migrate/20210115194447_create_rooms.rb
+++
@@ -1,9 +0,0 @@
-class CreateRooms < ActiveRecord::Migration[6.0]
-  def change
-    create_table :rooms do |t|
-      t.string  :artist_name, null: false
-      t.integer :genre_id, null: false
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20210126085416_create_room_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoomUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :room_users do |t|
-      t.references :user, foreign_keys: true
-      t.references :room, foreign_keys: true
-      t.timestamps
-    end
-  end
-end
